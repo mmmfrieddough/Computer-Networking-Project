714f1bfc7993766433d0876d9af1f75d80041936
PatSpagnardi-Anidea <pats@anidea-engineering.com>
2018-10-18 04:14:36 -0400

    Added access to configuration for individual peers

---
+++ src/fileIO/peerConfig.java
@@ -0,0 +1,28 @@
+package fileIO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class peerConfig {
+	public int ID;
+	public String host;
+	public int port;
+	public boolean hasFile;
+	
+	peerConfig(int newID) throws FileNotFoundException {
+		File file = new File("PeerInfo.cfg"); 
+	    Scanner sc = new Scanner(file);
+	    
+	    while(sc.hasNext()) {
+	    	if (sc.findInLine(Integer.toString(newID)) != null) {
+	    		this.ID = newID;
+	    		this.host = sc.next();
+	    		this.port = sc.nextInt();
+	    		this.hasFile = sc.nextInt() == 1;
+	    		break;
+	    	}
+	    	sc.nextLine();
+	    }
+	}
+}
