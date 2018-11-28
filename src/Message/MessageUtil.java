36bad12359d4dd5783ae19bce8351642e9f069c6
mmmfrieddough <mmmfrieddough@gmail.com>
2018-11-28 17:50:16 -0500

    Fixed bytetobit

--- src/Message/MessageUtil.java
+++ src/Message/MessageUtil.java
@@ -68,13 +68,14 @@ public class MessageUtil {
 	}
 	
 	public static BitSet fromByteArraytoBitSet(byte[] bytes) {
-	    BitSet bits = new BitSet();
-	    for (int i = 0; i < bytes.length * 8; i++) {
-	      if ((bytes[bytes.length - i / 8 - 1] & (1 << (i % 8))) > 0) {
-	        bits.set(i);
-	      }
-	    }
-	    return bits;
+		return BitSet.valueOf(bytes);
+//	    BitSet bits = new BitSet();
+//	    for (int i = 0; i < bytes.length * 8; i++) {
+//	      if ((bytes[bytes.length - i / 8 - 1] & (1 << (i % 8))) > 0) {
+//	        bits.set(i);
+//	      }
+//	    }
+//	    return bits;
 	  }
 
 	public static byte[] fromBitSettoByteArray(BitSet bitset) {
