e015f17650baf8001f59785fdf9a6e94000ef1dc
ftsai231 <f84024048@gmail.com>
2018-11-28 21:31:13 -0500

    cancel exit

--- src/connection/connectionPeer.java
+++ src/connection/connectionPeer.java
@@ -244,7 +244,7 @@ public class connectionPeer {
 			}
 			if (newFlag) {
 				System.out.println("Shutting down");
-				System.exit(0);
+//				System.exit(0);
 				peerProcess.setFinished();
 				break;
 			}
